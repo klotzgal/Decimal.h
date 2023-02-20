217a008f0dbe92f74c72006bac4c92992ad7d90b
Pickling Jacqulyn <pickling@pr-i5.kzn.21-school.ru>
2023-02-20 03:25:45 +0300

    float to dec| with comments

--- src/Convertation/s21_convertation.c
+++ src/Convertation/s21_convertation.c
@@ -1,31 +1,24 @@
 #include "s21_convertation.h"
 
-int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-  // не работает с INT_MIN
-  int result = CONV_OK;
-  if (dst) {
-    if (src < 0) {
-      src = -src;
-      set_sign(dst, '-');
-    };
-    dst->bits[LOW] = src;
-  } else {
-    result = CONV_ERR;
-  }
-  return result;
+/* функции, которые печатают*/
+void print_bytes(unsigned char num) {
+  for (unsigned char i = 1 << 7; i > 0; i = i / 2)
+    (num & i) ? printf("1") : printf("0");
+  printf("\n");
 }
 
-int s21_from_decimal_to_int(s21_decimal src, int *dst) {
-  // обрезаем дробную часть
-  int result = CONV_OK;
-  if (src.bits[LOW] < INT_MAX && src.bits[LOW] > INT_MAX) {
-    int sign = (check_bit(src, 127) == 1) ? -1 : 1;
-    *dst = src.bits[LOW] * sign;
-  } else {
-    result = CONV_ERR;
+void print_32(unsigned int a) {
+  printf("check bit\n");
+  printf("%3s|%21s|%68s|\n", "sign", "exp", "mantiss");
+  for (int i = 31; i >= 0; i--) {
+    printf("%3d", i);
   }
-  return result;
-  // ошибка если число выходит за пределы INT
+  printf("\n");
+  for (int i = 31; i >= 0; i--) {
+    printf("%3d", checkbit32(a, i));
+    if (i == 23) printf(" ");
+  }
+  printf("\n");
 }
 
 void print_dec(s21_decimal dec) {
@@ -42,6 +35,7 @@ void print_dec(s21_decimal dec) {
   }
 }
 
+/* вспомогательные функции*/
 unsigned int mask(unsigned int index) { return 1 << (index % 32); }
 
 int check_bit(s21_decimal dec, unsigned int index) {
@@ -56,32 +50,117 @@ void set_bit(s21_decimal *dec, unsigned int index) {
   dec->bits[POSITION] = dec->bits[POSITION] | mask(index);
 }
 
-void switch_bit(s21_decimal *dec, unsigned int index) {
-  dec->bits[POSITION] = dec->bits[POSITION] ^ mask(index);
-}
+// void switch_bit(s21_decimal *dec, unsigned int index) {
+//   dec->bits[POSITION] = dec->bits[POSITION] ^ mask(index);
+// }
+
 void set_sign(s21_decimal *dec, char sign) {
   if (sign == '+') unset_bit(dec, 127);
   if (sign == '-') set_bit(dec, 127);
 }
 
-// int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-//   if (src < 0) {
-//     set_sign(dst, '-');
-//     src = -src;
-//   };
-//   converter src2 = {src};
-// }
+int checkbit32(const int value, const int position) {
+  return ((value & (1 << position)) != 0);
+}
+
+void init_decimal(s21_decimal *dec) {
+  dec->bits[LOW] = 0u;
+  dec->bits[MID] = 0u;
+  dec->bits[HIGH] = 0u;
+  dec->bits[SCALE] = 0u;
+}
+
+/*целое в децимал*/
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+  // не работает с INT_MIN
+  int result = CONV_OK;
+  if (dst) {
+    if (src < 0) {
+      src = -src;
+      set_sign(dst, '-');
+    };
+    dst->bits[LOW] = src;
+  } else {
+    result = CONV_ERR;
+  }
+  return result;
+}
+/*из децимал в целое*/
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+  // обрезаем дробную часть
+  int result = CONV_OK;
+  if (src.bits[LOW] < INT_MAX && src.bits[LOW] > INT_MAX) {
+    int sign = (check_bit(src, 127) == 1) ? -1 : 1;
+    *dst = src.bits[LOW] * sign;
+  } else {
+    result = CONV_ERR;
+  }
+  return result;
+  // ошибка если число выходит за пределы INT
+}
+
+/* число с плавающей точкой в децимал */
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+  init_decimal(dst);
+  int result = CONV_OK;
+  char scale = 0;
+  printf("my num1 = %f\n", src);
+  if (src < 0) {
+    set_sign(dst, '-');
+    src = -src;
+  };
+
+  int exp = get_exp(src);
+  double tmp = (double)src;
+  // достали экспоненту
+
+  if (exp >= 95 || exp <= -96) {
+    result = CONV_ERR;
+  } else {
+    for (; (int)tmp == 0 && scale < 29; scale++) tmp *= 10;
+    // нормализовали число
+    for (int prec = 0; scale < 29 && prec < 6; scale++, prec++) tmp *= 10;
+    // работаем с точностью
+    // сейчас мы получили нужное нам целое число, от остальных чисел можем
+    // избавляться. Нам нужно только 7 значящих цифр, 1 значящую цифру мы
+    // получили после нормализации
+
+    tmp = round(tmp);
+    // округляем до ближайшего целого
+    for (; fmod(tmp, 10) == 0 && scale > 0; scale--) tmp /= 10;
+    // если число и до этого не нуждалось в увиличении точности, то мы его
+    // урезаем. При этом помним, что scale не может быть меньше 0
+    // теперь число у нас целое и только со значимыми числами
+    // так как цифр значимых только 7 оно должно поместиться в unsigned int
+    set_scale(dst, scale);
+    // устанавливаем степень 10 в decimal
+    s21_from_int_to_decimal((unsigned int)tmp, dst);
+  };
+  return result;
+}
+
+void set_scale(s21_decimal *dec, char power) {
+  converter scale = {dec->bits[SCALE]};
+  scale.bytes[2] = power;
+  dec->bits[SCALE] = scale.number;
+}
+
+int get_exp(float num) {
+  converter_float num_int = {num};
+  converter exp = {num_int.conv_fl << 1};
+  return (int)exp.bytes[3] - 127;
+}
 
 int main(void) {
-  int a = 100;
-  int b = 0;
-  printf("%d\n", b);
-  char str[100] = "";
+  float s = 0.00123456789;
+  printf("%d", get_exp(s));
+  converter_float num_int = {s};
+  print_32(num_int.conv_fl);
+  printf("get exp %d\n", get_exp(s));
+  printf("%.12f", s);
+  // unsigned char a = 123;
   s21_decimal dec = {0};
+  s21_from_float_to_decimal(s, &dec);
   print_dec(dec);
-  s21_from_int_to_decimal(a, &dec);
-  print_dec(dec);
-  s21_from_decimal_to_int(dec, &b);
-  printf("%d\n", b);
   return 0;
 }
--- src/Convertation/s21_convertation.h
+++ src/Convertation/s21_convertation.h
@@ -2,6 +2,7 @@
 #define S21_CONVARTATION_H
 
 #include <limits.h>
+#include <math.h>
 #include <stdio.h>
 
 #define MAX_DEC 79228162514264337593543950335
@@ -11,7 +12,7 @@
 #define CONV_OK 0
 #define CONV_ERR 1
 
-typedef enum { NORM, INF, N_INF, NAN, DOP_CODE } value_type_t;
+typedef enum { NORM, INF, N_INF, NANa, DOP_CODE } value_type_t;
 
 typedef struct {
   unsigned int bits[4];
@@ -19,9 +20,15 @@ typedef struct {
 } s21_decimal;
 
 typedef union {
-  int number;
-  char bytes[4];
+  unsigned int number;
+  unsigned char bytes[4];
 } converter;
+// так как бы нельзя делать, но я делаю....
+
+typedef union {
+  float fl;
+  unsigned int conv_fl;
+} converter_float;
 
 enum bit { LOW, MID, HIGH, SCALE };
 
@@ -29,12 +36,18 @@ int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_decimal_to_int(s21_decimal src, int *dst);
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
 
-void print_dec(s21_decimal dec);
+void init_decimal(s21_decimal *dec);
 unsigned int mask(unsigned int index);
 int check_bit(s21_decimal dec, unsigned int index);
 void unset_bit(s21_decimal *dec, unsigned int index);
 void set_bit(s21_decimal *dec, unsigned int index);
-void switch_bit(s21_decimal *dec, unsigned int index);
+// void switch_bit(s21_decimal *dec, unsigned int index);
 void set_sign(s21_decimal *dec, char sign);
+int get_exp(float num);
+void set_scale(s21_decimal *dec, char power);
+
+void print_bytes(unsigned char num);
+void print_32(unsigned int a);
+void print_dec(s21_decimal dec);
 
 #endif  // S21_CONVARTATION_H
